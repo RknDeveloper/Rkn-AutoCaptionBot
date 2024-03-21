755224b094e9c0f7fb4630deae738ce1b945655e
Rkn Developer <138338185+RknDeveloper@users.noreply.github.com>
2024-03-21 22:20:04 +0545

    Update Caption.py

--- Rkn_Bots/Caption.py
+++ Rkn_Bots/Caption.py
@@ -64,7 +64,7 @@ async def start_cmd(bot, message):
         f"<b>Hey, {message.from_user.mention}\n\nI'm an auto-caption bot. I automatically edit captions for videos, audio files, and documents posted on channels.\n\nuse <code>/set_caption</code> to set caption\nUse<code>/delcaption</code> To delete caption and set caption to default.\n\nNote:All commands works on channels only</b>",
         reply_markup=types.InlineKeyboardMarkup([[
             types.InlineKeyboardButton('Uᴩᴅᴀᴛᴇꜱ', url='https://t.me/RknDeveloper'),
-            types.InlineKeyboardButton('Sᴜᴩᴩᴏʀᴛ', url='https://t.me/RknDeveloperSupport')
+            types.InlineKeyboardButton('Sᴜᴩᴩᴏʀᴛ', url='https://t.me/Rkn_Bots_Support')
             ],[
             types.InlineKeyboardButton('🔥 Source Code 🔥', url='https://github.com/RknDeveloper/Rkn-AutoCaptionBot')
     ]]))
