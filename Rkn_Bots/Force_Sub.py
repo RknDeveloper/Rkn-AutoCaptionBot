3d85a37d106f39ad869fef9d205a211bccca7dc5
Rkn Developer <138338185+RknDeveloper@users.noreply.github.com>
2024-03-21 22:24:31 +0545

    Update To V0.0.5 🔥

--- Rkn_Bots/Force_Sub.py
+++ Rkn_Bots/Force_Sub.py
@@ -2,7 +2,7 @@ from pyrogram import Client, filters, enums
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from pyrogram.errors import UserNotParticipant
 from config import Rkn_Bots as Config
-from .database import *
+from .database import insert
 
 async def not_subscribed(_, client, message):
     user_id = int(message.from_user.id)
