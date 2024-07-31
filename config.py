cf9e9d01a9b5b89bf83a51243b876b570f9b2240
Rkn Developer <138338185+RknDeveloper@users.noreply.github.com>
2024-07-31 20:23:38 +0545

    Updated This File (Fixed...)

--- config.py
+++ config.py
@@ -1,44 +1,45 @@
 # (c) @RknDeveloperr
 # Rkn Developer 
 # Don't Remove Credit 😔
-# Telegram Channel @RknDeveloper & @Rkn_Bots
+# Telegram Channel @RknDeveloper & @Rkn_Botz
 # Developer @RknDeveloperr
 
 import re, os, time
 id_pattern = re.compile(r'^.\d+$') 
 
 class Rkn_Bots(object):
-    # Rkn client config
+    
+    # Rkn client config  ( required.. 😥)
     API_ID = os.environ.get("API_ID", "")
     API_HASH = os.environ.get("API_HASH", "")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 
-    #start_pic
+    # start_pic
     RKN_PIC = os.environ.get("RKN_PIC", "https://telegra.ph/file/21a8e96b45cd6ac4d3da6.jpg")
-
 
     # wes response configuration
     BOT_UPTIME = time.time()
     PORT = int(os.environ.get("PORT", "8080"))
-    FORCE_SUB = os.environ.get("FORCE_SUB", "") 
+
+    # force subs channel ( required.. 😥)
+    FORCE_SUB = os.environ.get("FORCE_SUB", "Rkn_Botz") 
     
-    # database config
+    # database config ( required.. 😥)
     DB_NAME = os.environ.get("DB_NAME", "AutoCaption_V05_Bot")     
     DB_URL = os.environ.get("DB_URL", "")
 
-    #caption
-    DEF_CAP = os.environ.get("DEF_CAP",
-                             "<b><a href='telegram.me/Rkn_Bots'>{file_name} Telegram : @RknDeveloper\n\nForward the file before Downloading.</a></b>",
+    # default caption 
+    DEF_CAP = os.environ.get("DEF_CAP", "<b><a href='https//:t.me/Rkn_Botz'>{file_name} Main Telegram Channel: @RknDeveloper</a></b>",
     )
 
-    #sticker Id
+    # sticker Id
     STICKER_ID = os.environ.get("STICKER_ID", "CAACAgIAAxkBAAELFqBllhB70i13m-woXeIWDXU6BD2j7wAC9gcAAkb7rAR7xdjVOS5ziTQE")
 
-    #admin id
+    # admin id  ( required.. 😥)
     ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '').split()]
     
 
 # Rkn Developer 
 # Don't Remove Credit 😔
-# Telegram Channel @RknDeveloper & @Rkn_Bots
+# Telegram Channel @RknDeveloper & @Rkn_Botz
 # Developer @RknDeveloperr
